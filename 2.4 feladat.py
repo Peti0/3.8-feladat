cade4d7c8eb7df2cee6d899a795a1ee91b766d5f
Peti0 <113418512+Peti0@users.noreply.github.com>
2022-11-21 16:10:07 +0100

    Add files via upload

---
+++ 2.4 feladat.py
@@ -0,0 +1,38 @@
+a="Lustaság"
+b="fél"
+c="egészség"
+print(a,b,c) #Működik
+
+
+
+print(6*(1-2))
+
+
+
+Pista = 6
+print(Pista+4)
+
+
+
+c = 10000
+r = 8
+m = 12
+t = int(input("Évek száma:"))
+FV = c*(1+(r/m))*m*t
+print(FV)
+
+
+
+x=int(input("Hány óra múlva csörögjön az óra?") )
+q=int(input("Aktuális idő"))
+z=q+x
+y=z/24
+if z > 24:
+    z=z-(y*24)
+print(z)
+
+
+
+
+
+
